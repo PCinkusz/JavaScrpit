d43118ab7dea02f3dce62455c65a56499b05cc3a
PCinkusz <pcinkusz123321@gmail.com>
2021-03-16 13:24:24 +0100

    mas

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const FileSaver = require('file-saver');
 const DATABASE_HOST='localhost';
 const DATABASE_USER='root';
 const DATABASE_PASSWORD='root';
-const DATABASE_NAME='baza';
+const DATABASE_NAME='baza2';
 
 const db = mysql.createConnection({
     host: DATABASE_HOST,
@@ -34,11 +34,16 @@ db.connect((err) => {
 })
 
 
-app.get('/showEventById/:id', (request, response) => {
-    let id = request.params.id
-    let sql = 'Select * from event where idEvent = ' +  mysql.escape(id)
+app.get('/insertPupil/<pupil_id>/<name>/<surname>/<pesel>/<classs>', (request, response) => {
+    let pupil_id = request.params.pupil_id
+    let name = request.params.name
+    let surname = request.params.surname
+    let pesel = request.params.pesel
+    let classs = request.params.class
+    let sql = 'INSERT INTO Pupil ( pupil_id, name, surname, pesel, classs ) VALUES ( ?,?,?,?,?) ', {values: [pupil_id,name,surname,pesel,classs]}
     db.query(sql, (err, result) => {
         if (err) throw err
-        response.send(response)
+        console.log("Record inserted")
     }) 
 })
+
